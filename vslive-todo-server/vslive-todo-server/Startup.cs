4d21083457f70bad6c3204d34cd5b8c23c084012
Matt Soucoup <msoucoup@codemilltech.com>
2017-09-05 16:46:55 -0500

    initial

---
+++ vslive-todo-server/vslive-todo-server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(vslive_todo_server.Startup))]
+
+namespace vslive_todo_server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
