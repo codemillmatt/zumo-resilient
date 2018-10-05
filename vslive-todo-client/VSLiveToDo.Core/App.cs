133fcfbee67c5cab1b3a142573c364d5f8f42c16
Matt Soucoup <msoucoup@codemilltech.com>
2018-10-05 09:00:41 -0700

    Initial add

--- vslive-todo-client/VSLiveToDo.Core/App.cs
+++ vslive-todo-client/VSLiveToDo.Core/App.cs
@@ -2,6 +2,12 @@
 using Xamarin.Forms;
 using Plugin.Settings;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
+using System.Collections.Generic;
+
 namespace VSLiveToDo.Core
 {
     public class App : Application
@@ -13,7 +19,43 @@ namespace VSLiveToDo.Core
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += (sender, e) =>
+                {
+
+                    // Add the notification message and title to the message
+                    var summary = $"Push notification received:" +
+                                        $"\n\tNotification title: {e.Title}" +
+                                        $"\n\tMessage: {e.Message}";
+
+                    // If there is custom data associated with the notification,
+                    // print the entries
+                    if (e.CustomData != null)
+                    {
+                        summary += "\n\tCustom data:\n";
+                        foreach (var key in e.CustomData.Keys)
+                        {
+                            summary += $"\t\t{key} : {e.CustomData[key]}\n";
+
+                            Analytics.TrackEvent("Push", new Dictionary<string, string> { { "push-key", $"{key}" } });
+                        }
+                    }
+
+                    // Send the notification summary to debug output
+                    System.Diagnostics.Debug.WriteLine(summary);
+                };
+            }
+
+
+            AppCenter.Start("ios=dcfb280a-b080-474b-8c3b-38baa5739d0f;"
+                            + "android=0418d688-810c-47f6-b532-7dbab8cdab5c",
+                            typeof(Analytics),
+                            typeof(Crashes),
+                            typeof(Push));
+
+            Push.SetEnabledAsync(true);
+
             OnResume();
         }
 
